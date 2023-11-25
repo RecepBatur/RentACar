36f142a66876535873ad406a4aee0f806d6e897e
Recep Batur <recep_btr44@hotmail.com>
2023-11-25 20:19:17 +0300

    Configure added.

--- Presentation/RentACar.WebApi/Program.cs
+++ Presentation/RentACar.WebApi/Program.cs
@@ -1,7 +1,19 @@
+using RentACar.Application.Features.CQRS.Handlers.AboutHandlers;
+using RentACar.Application.Interfaces;
+using RentACar.Persistence.Context;
+using RentACar.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<RentACarContext>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
