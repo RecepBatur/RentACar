1e6205f616dcb71cf0df22b18aa5f648f644d3d7
Recep Batur <recep_btr44@hotmail.com>
2023-11-29 01:19:01 +0300

    Location Api added.

---
+++ Presentation/RentACar.WebApi/Controllers/LocationsController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Application.Features.Mediator.Commands.LocationCommands;
+using RentACar.Application.Features.Mediator.Queries.LocationQueries;
+
+namespace RentACar.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LocationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> LocationList()
+        {
+            var values = await _mediator.Send(new GetLocationQuery());
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLocation(int id)
+        {
+            var value = await _mediator.Send(new GetLocationByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateLocation(CreateLocationCommand createLocationCommand)
+        {
+            await _mediator.Send(createLocationCommand);
+            return Ok("Lokasyon Başarılı Bir Şekilde Eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveLocation(int id)
+        {
+            await _mediator.Send(new RemoveLocationCommand(id));
+            return Ok("Lokasyon Başarılı Bir Şekilde Silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateLocation(UpdateLocationCommand updateLocationCommand)
+        {
+            await _mediator.Send(updateLocationCommand);
+            return Ok("Lokasyon Başarılı Bir Şekilde Güncellendi");
+        }
+    }
+}
