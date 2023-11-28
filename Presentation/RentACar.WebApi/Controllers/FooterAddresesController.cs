66970b4cd6f7e606e511b7ef8276d6cd56370410
Recep Batur <recep_btr44@hotmail.com>
2023-11-29 00:31:34 +0300

    FooterAddress Api added.

---
+++ Presentation/RentACar.WebApi/Controllers/FooterAddresesController.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Application.Features.Mediator.Commands.FooterAddressCommands;
+using RentACar.Application.Features.Mediator.Queries.FooterAddressQueries;
+
+namespace RentACar.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooterAddresesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FooterAddresesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> FooterAddressList()
+        {
+            var value = await _mediator.Send(new GetFooterAddressQuery());
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFooterAddress(CreateFooterAddressCommand createFooterAddressCommand)
+        {
+            await _mediator.Send(createFooterAddressCommand);
+            return Ok("Adres Bilgisi Başarılı Bir Şekilde Eklendi");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFooterAddress(int id)
+        {
+            var value = await _mediator.Send(new GetFooterAddressByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveFooterAddress(int id)
+        {
+            await _mediator.Send(new RemoveFooterAddressCommand(id));
+            return Ok("Adres Bilgisi Başarılı Bir Şekilde Silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateFooterAddress(UpdateFooterAddressCommand updateFooterAddressCommand)
+        {
+            await _mediator.Send(updateFooterAddressCommand);
+            return Ok("Adres Bilgisi Başarılı Bir Şekilde Güncellendi");
+        }
+    }
+
+}
