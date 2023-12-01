844ecdee02ed016595066976cf4e8ba8a3920a13
Recep Batur <recep_btr44@hotmail.com>
2023-12-01 21:50:41 +0300

    About api consume.

---
+++ Frontends/RentACar.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.AboutDtos;
+
+namespace RentACar.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _AboutUsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient(); // istekte bulunabilmek için bir istemci oluşturdum.
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // responseMessage içeriğinden gelen api'de ki veriyi string formatta oku.
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
