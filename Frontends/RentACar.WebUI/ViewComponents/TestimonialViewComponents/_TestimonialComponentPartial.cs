f2dd46337ef919fac86370a63a42af45f0ee6f1e
Recep Batur <recep_btr44@hotmail.com>
2023-12-01 22:02:21 +0300

    about becomeADriver api consume

---
+++ Frontends/RentACar.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace RentACar.WebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                //var values = JsonConvert.DeserializeObject<List<>>(jsonData);
+                //return View(values);
+            }
+            return View();
+        }
+    }
+}
