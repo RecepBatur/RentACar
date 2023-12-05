8fc6e571bff3900378aaebb58178acb93d979c9c
Recep Batur <recep_btr44@hotmail.com>
2023-12-06 00:08:18 +0300

    Car list.

---
+++ Frontends/RentACar.WebUI/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.CarDtos;
+
+namespace RentACar.WebUI.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<CarListDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
