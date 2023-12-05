cdc7d279e2082df6f9c64fdac91822185bdf7763
Recep Batur <recep_btr44@hotmail.com>
2023-12-05 23:46:12 +0300

    Services list.

---
+++ Frontends/RentACar.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.ServiceDtos;
+using RentACar.Dto.TestimonialDtos;
+
+namespace RentACar.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ServiceListDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
