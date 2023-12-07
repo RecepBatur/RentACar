a51e85fbdb800fc1d44de97ed57ce8fe063cdd17
Recep Batur <recep_btr44@hotmail.com>
2023-12-07 18:16:55 +0300

    Contact and Footer address list.

---
+++ Frontends/RentACar.WebUI/ViewComponents/FooterAddressComponents/_FooterAddressComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.AboutDtos;
+using RentACar.Dto.FooterAddressDtos;
+
+namespace RentACar.WebUI.ViewComponents.FooterAddressComponents
+{
+    public class _FooterAddressComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterAddressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var client = _httpClientFactory.CreateClient(); // istekte bulunabilmek için bir istemci oluşturdum.
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/FooterAddreses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // responseMessage içeriğinden gelen api'de ki veriyi string formatta oku.
+                var values = JsonConvert.DeserializeObject<List<FooterAddressListDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
