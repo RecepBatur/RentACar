5496dce2d198a2e8027cdcdb6332bea53d455e86
Recep Batur <recep_btr44@hotmail.com>
2023-12-17 17:54:34 +0300

    Listing featured vehicles on the home page.

---
+++ Frontends/RentACar.WebUI/ViewComponents/DefaultViewComponents/_DefaultLastFiveCarsWithBrandsComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.CarDtos;
+using RentACar.Dto.TestimonialDtos;
+
+namespace RentACar.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultLastFiveCarsWithBrandsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultLastFiveCarsWithBrandsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Cars/GetLastFiveCarWithBrandQueryHandler");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLastFiveCarsWithBrandsDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
