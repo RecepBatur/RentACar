adf93b7cb528af5cb961a6e8bfbf5f7dde644689
Recep Batur <recep_btr44@hotmail.com>
2023-12-17 18:09:50 +0300

    Services list with home page.

---
+++ Frontends/RentACar.WebUI/ViewComponents/ServiceViewComponents/_ServiceComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.ServiceDtos;
+
+namespace RentACar.WebUI.ViewComponents.ServiceViewComponents
+{
+    public class _ServiceComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServiceComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ServiceListDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
