4716c910c9b436eafa8acb377f9849dfeea1ee64
Recep Batur <recep_btr44@hotmail.com>
2024-01-16 23:43:34 +0300

    Blog details last blogs list.

--- Frontends/RentACar.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsRecentBlogsComponentPartial.cs
+++ Frontends/RentACar.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsRecentBlogsComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.BlogDtos;
 
 namespace RentACar.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsRecentBlogsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsRecentBlogsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Blogs/GetLastThreeBlogsWithAuthorsList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLastThreeBlogsWithAuthorsDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
