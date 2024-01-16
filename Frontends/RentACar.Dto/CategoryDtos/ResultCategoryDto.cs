b93d54cc5387f56bae036c33e9e3475a586066a9
Recep Batur <recep_btr44@hotmail.com>
2024-01-16 23:38:39 +0300

    Blog details category list.

---
+++ Frontends/RentACar.Dto/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Dto.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
--- Frontends/RentACar.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponentPartial.cs
+++ Frontends/RentACar.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.CategoryDtos;
+using RentACar.Dto.ServiceDtos;
 
 namespace RentACar.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsCategoryViewComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsCategoryViewComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
