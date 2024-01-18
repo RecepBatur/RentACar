fd7fb6781156c9672653aff3cf45ea631cfe67e5
Recep Batur <recep_btr44@hotmail.com>
2024-01-18 17:49:55 +0300

    Blog Details author information listed

--- Frontends/RentACar.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsAuthorAboutComponentPartial.cs
+++ Frontends/RentACar.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsAuthorAboutComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.AuthorDtos;
+using RentACar.Dto.TagCloudDtos;
+using System.Net.Http;
 
 namespace RentACar.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsAuthorAboutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _BlogDetailsAuthorAboutComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            ViewBag.blogid = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7286/api/Blogs/GetBlogsByAuthorId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<GetAuthorByBlogAuthorIdDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
