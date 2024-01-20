1f7d00cecc5793c976177684807d80ba5686b38a
Recep Batur <recep_btr44@hotmail.com>
2024-01-20 18:53:04 +0300

    Admin panel car list.

--- Frontends/RentACar.WebUI/Controllers/AdminCarController.cs
+++ Frontends/RentACar.WebUI/Controllers/AdminCarController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.CarDtos;
 
 namespace RentACar.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<CarListDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
