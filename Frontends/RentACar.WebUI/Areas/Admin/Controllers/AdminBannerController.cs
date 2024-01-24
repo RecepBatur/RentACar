f956192b104cf99f30b8349ed09d8836c6de9586
Recep Batur <recep_btr44@hotmail.com>
2024-01-24 16:13:13 +0300

    Admin banner list and add transactions.

--- Frontends/RentACar.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
+++ Frontends/RentACar.WebUI/Areas/Admin/Controllers/AdminBannerController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RentACar.Dto.BannerDtos;
-using RentACar.Dto.BrandDtos;
+using System.Text;
 
 namespace RentACar.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("Admin/AdminBanner")] //Bu controller üzerinde tanımlanan metotların tamamı bu url'i alarak gidecek.
     public class AdminBannerController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
@@ -14,7 +15,7 @@ namespace RentACar.WebUI.Areas.Admin.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-
+        [Route("Index")]
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
@@ -24,6 +25,37 @@ namespace RentACar.WebUI.Areas.Admin.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("CreateBanner")]
+        public IActionResult CreateBanner()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateBanner")]
+        public async Task<IActionResult> CreateBanner(CreateBannerDto createBannerDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBannerDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7286/api/Banners", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBanner", new {area = "Admin"});
+            }
+            return View();
+        }
+        [Route("RemoveBanner/{id}")]
+        public async Task<IActionResult> RemoveBanner(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7286/api/Banners?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminBanner", new { area = "Admin" });
             }
             return View();
         }
