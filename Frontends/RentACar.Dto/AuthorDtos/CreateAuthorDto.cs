a8eeaa173f9e296ca2616c888b1fd82d3a0e23fa
Recep Batur <recep_btr44@hotmail.com>
2024-01-24 18:44:20 +0300

    Admin author crud transactions.

---
+++ Frontends/RentACar.Dto/AuthorDtos/CreateAuthorDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Dto.AuthorDtos
+{
+    public class CreateAuthorDto
+    {
+        public string AuthorName { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Frontends/RentACar.Dto/AuthorDtos/UpdateAuthorDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Dto.AuthorDtos
+{
+    public class UpdateAuthorDto
+    {
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
