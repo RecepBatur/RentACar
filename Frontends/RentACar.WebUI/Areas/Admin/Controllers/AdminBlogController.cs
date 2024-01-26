6b08669272c19879ef613bd2d766bd71079fa1d1
Recep Batur <recep_btr44@hotmail.com>
2024-01-26 16:42:45 +0300

    Admin create, update canceled.

--- Frontends/RentACar.WebUI/Areas/Admin/Controllers/AdminBlogController.cs
+++ Frontends/RentACar.WebUI/Areas/Admin/Controllers/AdminBlogController.cs
@@ -28,59 +28,11 @@ namespace RentACar.WebUI.Areas.Admin.Controllers
             }
             return View();
         }
-        [HttpGet]
-        [Route("CreateBlog")]
-        public IActionResult CreateBlog()
-        {
-            return View();
-        }
-        [HttpPost]
-        [Route("CreateBlog")]
-        public async Task<IActionResult> CreateBlog(CreateBlogDto createBlogDto)
-        {
-            var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(createBlogDto);
-            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PostAsync("https://localhost:7286/api/Blogs", content);
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                return RedirectToAction("Index", "AdminBlog", new { area = "Admin" });
-            }
-            return View();
-        }
         [Route("RemoveBlog/{id}")]
         public async Task<IActionResult> RemoveBlog(int id)
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.DeleteAsync($"https://localhost:7286/api/Blogs?id=" + id);
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                return RedirectToAction("Index", "AdminBlog", new { area = "Admin" });
-            }
-            return View();
-        }
-        [HttpGet]
-        [Route("UpdateBlog/{id}")]
-        public async Task<IActionResult> UpdateBlog(int id)
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7286/api/Blogs/{id}");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jesonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<UpdateBlogDto>(jesonData);
-                return View(values);
-            }
-            return View();
-        }
-        [HttpPost]
-        [Route("UpdateBlog/{id}")]
-        public async Task<IActionResult> UpdateBlog(UpdateBlogDto updateBlogDto)
-        {
-            var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(updateBlogDto);
-            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PutAsync("https://localhost:7286/api/Blogs/", stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index", "AdminBlog", new { area = "Admin" });
