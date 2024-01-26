05187f662346d2b5ee44c466b275408fe1e910df
Recep Batur <recep_btr44@hotmail.com>
2024-01-26 17:30:29 +0300

    Admin blogId commentList api added.

---
+++ Frontends/RentACar.WebUI/Areas/Admin/Controllers/AdminCommentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.CommentDtos;
+
+namespace RentACar.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminComment")]
+    public class AdminCommentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCommentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("Index/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/Comments/CommentListByBlog?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
