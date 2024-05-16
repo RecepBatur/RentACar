0feec513ef62b410c4bb3f4dd246eff6d88328c2
Recep Batur <recep_btr44@hotmail.com>
2024-05-16 19:32:46 +0300

    Admin SocialMedia crud transactions.

---
+++ Frontends/RentACar.WebUI/Areas/Admin/Controllers/AdminSocialMediaController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.Dto.SocialMediaDtos;
+using System.Text;
+
+namespace RentACar.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminSocialMedia")]
+    public class AdminSocialMediaController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminSocialMediaController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7286/api/SocialMedias");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<SocialMediaListDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("CreateSocialMedia")]
+        public IActionResult CreateSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("CreateSocialMedia")]
+        public async Task<IActionResult> CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSocialMediaDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7286/api/SocialMedias", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminSocialMedia", new { area = "Admin" });
+            }
+            return View();
+        }
+        [Route("RemoveSocialMedia/{id}")]
+        public async Task<IActionResult> RemoveSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7286/api/SocialMedias?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminSocialMedia", new { area = "Admin" });
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("UpdateSocialMedia/{id}")]
+        public async Task<IActionResult> UpdateSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7286/api/SocialMedias/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jesonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateSocialMediaDto>(jesonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        [Route("UpdateSocialMedia/{id}")]
+        public async Task<IActionResult> UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateSocialMediaDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7286/api/SocialMedias/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminSocialMedia", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
